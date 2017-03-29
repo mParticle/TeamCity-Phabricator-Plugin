5298126b614a7d6727d1dc3e138798044fa19921
mbenua-mparticle <mbenua@mparticle.com>
2017-03-29 16:15:33 -0700

    Removing deliberately failing test.

--- common/src/test/java/com/couchmate/teamcity/phabricator/clients/ConduitClientTest.java
+++ common/src/test/java/com/couchmate/teamcity/phabricator/clients/ConduitClientTest.java
@@ -1,7 +1,5 @@
 package com.couchmate.teamcity.phabricator.clients;
 
-import com.couchmate.teamcity.phabricator.DifferentialReview;
-import jetbrains.buildServer.log.Loggers;
 import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 
@@ -18,19 +16,4 @@ public final class ConduitClientTest extends TestCase {
     public void testPassing() {
         assertTrue(true);
     }
-
-    @Test
-    public void testGetDiffDetails() {
-
-        ConduitClient client = new ConduitClient(
-                "phabricator.corp.mparticle.com",
-                "https",
-                "123456", Loggers.AGENT);
-
-        DifferentialReview review = new DifferentialReview(client);
-        boolean result = review.fetchReviewData("15");
-        assertTrue(result);
-    }
-
-
 }
